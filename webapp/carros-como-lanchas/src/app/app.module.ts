1febea551556b9c946d3a01cf10cf5acb7f6e6a2
Roberto Moure <robertosmoure@gmail.com>
2020-03-29 17:45:42 -0300

    [thebertful] Inicialização de components

--- webapp/carros-como-lanchas/src/app/app.module.ts
+++ webapp/carros-como-lanchas/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './shared/components/menu-bar/menu-bar.component';
+import { BottomBarComponent } from './shared/components/bottom-bar/bottom-bar.component';
+import { ItemCardComponent } from './shared/components/item-card/item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BottomBarComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule
