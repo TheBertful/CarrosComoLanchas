97f6c64db36870ac3ed48e50ac415a5001b4919c
Roberto Moure <robertosmoure@gmail.com>
2020-03-29 17:56:21 -0300

    [thebertful] Inicialização de componentes de conteúdo

--- webapp/carros-como-lanchas/src/app/app.module.ts
+++ webapp/carros-como-lanchas/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './shared/components/menu-bar/menu-bar.component';
 import { BottomBarComponent } from './shared/components/bottom-bar/bottom-bar.component';
 import { ItemCardComponent } from './shared/components/item-card/item-card.component';
+import { GameComponent } from './content/components/game/game.component';
+import { InstructionsComponent } from './content/components/instructions/instructions.component';
+import { HomeComponent } from './content/components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     BottomBarComponent,
-    ItemCardComponent
+    ItemCardComponent,
+    GameComponent,
+    InstructionsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
